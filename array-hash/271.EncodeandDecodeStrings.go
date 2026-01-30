bf9d19b0ac37685c05f1fb393a7ea18c31a6ed4a
kyyril <khairilrahman55@gmail.com>
2026-01-30 17:04:24 +0700

    feat(leetcode): add encode and decode strings solution

---
+++ array-hash/271.EncodeandDecodeStrings.go
@@ -0,0 +1,58 @@
+// Length-Prefix Encoding
+// Used in:
+// network protocols
+// databases
+// message queues
+// real backend systems
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	codec := Constructor()
+	encoded := codec.Encode([]string{"hello", "world","team"})
+	decoded := codec.Decode(encoded)
+	fmt.Println(encoded)
+	fmt.Println(decoded)
+}
+type Codec struct {
+}
+
+func Constructor () Codec{
+	return Codec{}
+}
+
+func (c *Codec) Encode(strs []string) string {
+	var res []byte
+	for _, s := range strs {
+		res = append(res, []byte(strconv.Itoa(len(s)))...)
+		res = append(res, '#')
+		res = append(res, []byte(s)...)
+	}
+	return string(res)
+}
+
+
+// 5#hello5#world
+// use pointer
+func (c *Codec) Decode(str string) []string {
+	var res = []string{}
+	i := 0
+	for i < len(str) {
+		j := i // 1
+		for str[j] != '#' {
+			j++
+		}
+		length, _ := strconv.Atoi(str[i:j]) // 1 -> 6
+		j++ //start after #
+		res = append(res, str[j:j+length])//append hello
+
+		i = j + length
+
+	}
+	return res
+}
