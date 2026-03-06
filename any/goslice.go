7e14f185a24dec101e426fac3221775f47d616c3
kyyril <khairilrahman55@gmail.com>
2026-03-06 14:05:27 +0700

    feat(goslice): add Go slice operation examples
    Demonstrate various slice operations including append with variadic,
    copy, len/cap, and element removal techniques in Go

--- any/goslice.go
+++ any/goslice.go
@@ -4,7 +4,29 @@ import "fmt"
 
 
 func main (){
-	nums := []int {1, 2, 3, 4, 5, 6, 7, 8}
-	last := len(nums)-1
-	fmt.Println(nums[:last])
-} 
+	slice := []int{1, 2}
+	slice = append(slice, 3, 4)
+	newSlice := []int{5, 6}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice) // [1 2 3 4 5 6]
+	fmt.Println("====append=====")
+
+	//copy
+	goal := make([]int, len(slice))
+	copy(goal, slice)
+	fmt.Println(goal) // [1 2 3 4 5 6]
+	fmt.Println("====copy=====")
+
+	//len() & cap()
+	s := make([]int, 3, 10)
+	fmt.Println(len(s)) // 3
+	fmt.Println(cap(s)) // 10
+	fmt.Println("====len&cap=====")
+
+	// remove i
+	i := 2
+	slice = append(slice[:i], slice[i+1:]...)
+	fmt.Println(slice)
+	fmt.Println("====remove[i]=====")
+
+}
--- tries/211. Design Add and Search Words Data Structure.go
+++ tries/211. Design Add and Search Words Data Structure.go
@@ -1,54 +1,56 @@
-package main
-
 type WordDictionary struct {
-	children [26]*WordDictionary
-	isEnd    bool
+    children [26]*WordDictionary
+    isEnd    bool
 }
 
 func Constructor() WordDictionary {
-	return WordDictionary{}
+    return WordDictionary{}
 }
 
 func (this *WordDictionary) AddWord(word string) {
-	curr := this
-	for i := 0; i < len(word); i++ {
-		index := word[i] - 'a'
-		if curr.children[index] == nil {
-			curr.children[index] = &WordDictionary{}
-		}
-		curr = this.children[index]
-	}
-	curr.isEnd = true
+    curr := this
+    for _, char := range word {
+        index := char - 'a'
+        if curr.children[index] == nil {
+            curr.children[index] = &WordDictionary{}
+        }
+        curr = curr.children[index]
+    }
+    curr.isEnd = true
 }
 
 func (this *WordDictionary) Search(word string) bool {
-	// Kita panggil fungsi helper rekursif mulai dari index 0
-	return this.dfs(0, word)
+    // Kita panggil fungsi helper rekursif mulai dari index 0
+    return this.dfs(0, word)
 }
 
 func (this *WordDictionary) dfs(index int, word string) bool {
-	curr := this
-	for i := 0; i < len(word); i++ {
-		char := word[i]
-		if char == '.' {
-			//backtracking: try all 26 char
-			for _, child := range curr.children {
-				
-			}
-		} else {
-			index := char - 'a'
-			if curr.children[index] == nil {
-				return false
-			}
-			curr = curr.children[index]
-		}
-	}
-	return curr.isEnd
+    curr := this
+    
+    for i := index; i < len(word); i++ {
+        char := word[i]
+        
+        if char == '.' {
+            // BACKTRACKING: Coba semua 26 kemungkinan huruf
+            for _, child := range curr.children {
+                if child != nil {
+                    // Jika jalur ini (mulai dari huruf selanjutnya) mengembalikan true
+                    if child.dfs(i+1, word) {
+                        return true
+                    }
+                }
+            }
+            // Jika setelah mencoba semua 26 huruf tidak ada yang cocok
+            return false
+        } else {
+            // Kasus huruf biasa (a-z)
+            idx := char - 'a'
+            if curr.children[idx] == nil {
+                return false
+            }
+            curr = curr.children[idx]
+        }
+    }
+    
+    return curr.isEnd
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * obj := Constructor();
- * obj.AddWord(word);
- * param_2 := obj.Search(word);
- */
