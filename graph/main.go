b27cc3643f77fe3a6c8eb6f1d2f12b91e3caec43
kyyril <khairilrahman55@gmail.com>
2026-03-27 08:37:23 +0700

    feat(graph): add Graph struct with AddVertex method

--- graph/main.go
+++ graph/main.go
@@ -1,4 +1,14 @@
 package main
+import "fmt"
+
+type Graph struct {
+	vertices []*Vertex
+}
+
+type Vertex struct {
+	key int
+	adjacent []*Vertex
+}
 
 // graph represent an adjacency list graph
 
@@ -6,4 +16,15 @@ package main
 // vertex represent an adjacency matrix graph
 
 // add vertex
+func (g *Graph) AddVertex(k int) {
+	g.vertices = append(g.vertices, &Vertex{key:k})
+}
 // add edge
+
+func main(){
+	test := &Graph{}
+	for i:=1; i <= 5; i++ {
+		test.AddVertex(i)
+	}
+	fmt.Println(test)
+}
